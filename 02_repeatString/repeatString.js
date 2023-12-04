d701862605f04d076ec3892572d5136761867196
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-04 14:44:48 +0000

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    output = '';
+    while (num--) {
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
