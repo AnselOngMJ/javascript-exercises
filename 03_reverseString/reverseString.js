7fa7333d59fbdcfc6e777a24c489c7eae275bf72
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-04 14:54:56 +0000

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let output = '';
+    const chars = word.split('');
+    chars.reverse();
+    for (const char of chars) {
+        output += char;
+    }
+    return output;
 };
 
 // Do not edit below this line
