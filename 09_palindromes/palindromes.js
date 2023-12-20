402ea1ca39314ae25cde8195d1752e201b48f4fe
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-20 15:47:55 +0000

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const s = str.toLowerCase();
+    const arr = [...s.matchAll(/[a-z]|[0-9]/g)].join('');
+    console.log(arr)
+    return arr === arr.split('').reverse().join('');
 };
 
 // Do not edit below this line
